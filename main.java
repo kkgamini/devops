c2be22f0085b070c284c9c1ef1f04a8946e0631e
root <root@test01.edureka.com>
2018-04-21 15:00:20 +0530

    tryfor merge

--- main.java
+++ main.java
@@ -2,4 +2,5 @@ i am for june
 i am on my way for clieNT a bit late
 i am for april
 i am on my way for clien
+ i have added this line to check for merging issues can be done or not
 i am done wih aprilt
